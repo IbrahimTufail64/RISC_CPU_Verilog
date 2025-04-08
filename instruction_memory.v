b1b2131c0b120a90d4629ae65fc2bcbfb4466b84
Ibrahim Tufail <ibrahimsomeonenotimportant@gmail.com>
2025-04-08 23:53:57 +0500

    saving before implementing possible breaking change: multi-cycle support

--- instruction_memory.v
+++ instruction_memory.v
@@ -22,7 +22,7 @@ initial begin
     register_memory[1] = 16'b1001_0101_0001_0110; // LOAD $r0 6($r1) : r0 = r1+6
     register_memory[2] = 16'b0101_0000_1000_0101;  // shift right
     register_memory[3] = 16'b0110_0101_1000_0111;  // shift left
-    register_memory[4] = 16'b1000_0000_0111_0010;  
+    register_memory[4] = 16'b1000_0000_0111_0010;  // branch if not equal   
     register_memory[5] = 16'b0101_0000_0001_0000;  
     register_memory[6] = 16'b0101_0000_0001_0010;  
     register_memory[7] = 16'b1011_0000_0000_0000; // jump to 0 instruction addr  
